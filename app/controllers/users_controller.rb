e0cd2b005b4d23253b9ebc9c43d73ea96651ca82
gigi <mmgamao1@gmail.com>
2020-03-16 13:01:27 -0700

    controller edits

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,31 +4,56 @@ class UsersController < ApplicationController
   def new
     @user = User.new
   end
-    
+
   def create
     @user = User.new(user_params)
-        
-  # store all emails in lowercase to avoid duplicates and case-sensitive login errors:
-    @user.email.downcase!
-        
     if @user.save
-    # If user saves in the db successfully:
+      # If user saves in the db successfully:
       flash[:notice] = "Account created successfully!"
-      redirect_to root_path
+      session[:user_id] = @user.id
+      redirect_to user_path
       else
-    # If user fails model validation - probably a bad password or duplicate email:
+      # If user fails model validation - probably a bad password or duplicate email:
       flash.now.alert = "Oops, couldn't create account. Please make sure you are using a valid email and password and try again."
       render :new
     end
   end
-    
-private
-    
-  def user_params
-  # strong parameters - whitelist of allowed fields #=> permit(:name, :email, ...)
-  # that can be submitted by a form to the user model #=> require(:user)
-    params.require(:user).permit(:name, :email, :password, :password_confirmation)
+
+  def show
   end
-      
-  # ----- end of added lines -----
+
+  def edit
+  end
+
+  def update
+    @user.update(user_params)
+    if @user.save
+      session[:user_id] = @user.id
+      redirect_to user_path(@user)
+    else
+      #error message
+      render 'users/#{@user.id}/edit'
+    end
+  end
+
+  def destroy
+    @user.destroy
+    session.delete :user_id
+    @current_user = nil
+    redirect_to root_path
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:username, :name, :password, :password_confirmation)
+  end
+
+  def set_user
+    @user = User.find_by(id: params[:id])
+  end
+
+  def require_login
+    return head(:forbidden) unless session.include? :user_id
+  end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,5 +3,8 @@ class User < ApplicationRecord
 
   has_secure_password
 
-	validates :email, presence: true, uniqueness: true
-end
+  validates :username, presence: true, uniqueness: true
+  validates :password, presence: true, on: :create
+  validates :email, presence: true, uniqueness: true
+
+  end
